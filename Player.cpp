5f84db7f9d9aef07176ba194deabae9c3dd6465d
k023g0121desktop <k023g0121@g.neec.ac.jp>
2023-11-06 23:48:27 +0900

    プレイヤーがジャンプした際の問題点を解消

--- Player.cpp
+++ Player.cpp
@@ -11,31 +11,42 @@ Player::~Player()
 
 void Player::Update(char* keys)
 {
-	// プレイヤーの下のブロックが地面の場合
-	if (MapData::map[LeftBottom_.y][LeftBottom_.x] != 0 || MapData::map[RightBottom_.y][RightBottom_.x] != 0) {
-		// 地面の上にいることにする
-		isPlayerOnGround_ = true; 
-		velocity_.y = 0.0f;
-		// 空中にいる場合はfalse
-	} else {
-		isPlayerOnGround_ = false;
-	}
-
 	// Wキーを押したかつ、地面の上にいる場合ジャンプする（初速度を与える）
 	if (keys[DIK_W] && isPlayerOnGround_) {
 		velocity_.y = 20.0f;
 	}
-
-	// プレイヤーのy座標の更新処理
-	pos_.y -= static_cast<int>(velocity_.y);
 
 	// プレイヤーが地面の上にいない場合は重力を受ける
 	if (!isPlayerOnGround_) {
 		velocity_.y -= accelerator_.y;
 	}
 
+	// プレイヤーのy座標の更新処理
+	pos_.y -= static_cast<int>(velocity_.y);
+
+	// プレイヤーのx座標の更新処理（デバッグ用）
+	pos_.x += 4;
+
 	// 4点のマップ座標を更新
 	UpdateMapPos();
+
+	// プレイヤーの下にブロックがある場合
+	if (MapData::map[LeftBottom_.y][LeftBottom_.x] != 0 || MapData::map[RightBottom_.y][RightBottom_.x] != 0) {
+
+		// 地面の上にいることにする
+		isPlayerOnGround_ = true;
+
+		// 埋まっている場合は押し戻す
+		while (MapData::map[LeftBottom_.y][LeftBottom_.x] != 0 || MapData::map[RightBottom_.y][RightBottom_.x] != 0) {
+			pos_.y--;
+
+			// 4点のマップ座標を更新
+			UpdateMapPos();
+		}
+		// 空中にいる場合はfalse
+	} else {
+		isPlayerOnGround_ = false;
+	}
 }
 
 void Player::Draw()
@@ -68,17 +79,17 @@ void Player::UpdateMapPos()
 	};
 
 	RightTop_ = {
-		(pos_.x + size_) / size_,
+		(pos_.x + size_ - 1) / size_,
 		pos_.y / size_
 	};
 
 	LeftBottom_ = {
 		pos_.x / size_,
-		(pos_.y + size_) / size_
+		(pos_.y + size_ - 1) / size_
 	};
 
 	RightBottom_ = {
-		(pos_.x + size_) / size_,
-		(pos_.y + size_) / size_
+		(pos_.x + size_ - 1) / size_,
+		(pos_.y + size_ - 1) / size_
 	};
 }
